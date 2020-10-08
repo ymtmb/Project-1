da8e5d9632dfe955677b125ed1eb829ba1de8bf3
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-10-07 23:49:54 -0500

    Final changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function validate() {
-    if (document.getElementById('tc').checked) {
+    if (document.getElementById('check').checked) {
         alert("By checking this box, you have read and understood the Terms & Conditions.");
     }
 }
