3e5a96f6ff8c98a6099328f1e28be050f0923729
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-10-28 12:44:25 -0500

    Added validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 var click = document.getElementById("check");
-check.onclick = function() {
+click.onclick = function() {
 	if (document.getElementById('check').checked) {
         alert("By checking this box, you have read and understood the Terms & Conditions.");
-    }
+}
+}
+
+document.getElementById("main").onsubmit = function() {
+  if (document.getElementById("UID").value == "") {
+    alert("UID Name Field is required");
+		return false;
+  }
+  if (document.getElementById("email").value == "") {
+    alert("Email Field is required");
+		return false;
+  }
 }
