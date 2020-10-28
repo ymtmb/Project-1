224cc882856c7819ccc485f1aed18d0748434210
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-10-28 12:36:51 -0500

    fixed the onlick and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-function validate() {
-    if (document.getElementById('check').checked) {
+var click = document.getElementById("check");
+check.onclick = function() {
+	if (document.getElementById('check').checked) {
         alert("By checking this box, you have read and understood the Terms & Conditions.");
     }
 }
